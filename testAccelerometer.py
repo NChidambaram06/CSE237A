30b08e02ab50d54693d3d381c02c260346910201
Kevin Zhang <kevinzhang425@gmail.com>
2025-03-04 04:32:51 +0000

    fixed mod

--- testAccelerometer.py
+++ testAccelerometer.py
@@ -279,7 +279,7 @@ if __name__ == "__main__":
         
         # v_roll, v_pitch = calculate_roll_pitch_yaw(v_x, v_y, v_z, 0, 0, 0, 0)
 
-        if n % 20:
+        if n % 20 == 0:
             print(f" Accel: ({a_x}, {a_y}, {a_z})")
             # print(f"accel Magnitude: {magnitude}")
             # print(f"roll(x): {roll}(y), pitch:{pitch}")
