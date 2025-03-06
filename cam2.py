19e6993b72694835e4d6c4c7094ab02e6af145f3
NChidambaram06 <57117575+NChidambaram06@users.noreply.github.com>
2025-03-05 22:39:28 -0800

    final demo files

---
+++ cam2.py
@@ -0,0 +1,77 @@
+import cv2
+import mediapipe
+import numpy as np
+import pyautogui
+from picamera2 import Picamera2
+
+# Initialize screen size
+width, height = pyautogui.size()
+print(f"Screen width: {width} pixels")
+print(f"Screen height: {height} pixels")
+
+# Initialize PiCamera2
+camera = Picamera2()
+camera.configure(camera.create_still_configuration())
+camera.start()
+
+# Initialize MediaPipe Hand Tracking
+drawingModule = mediapipe.solutions.drawing_utils
+handsModule = mediapipe.solutions.hands
+
+hands = handsModule.Hands(static_image_mode=False, 
+                        min_detection_confidence=0.7, 
+                        min_tracking_confidence=0.7, 
+                        max_num_hands=2)
+def norm(x):
+    return (x - 0.2)/(0.8-0.2)
+def normX(x):
+    return (x-0.4)/(0.6-0.4)
+def getXY():
+    # Capture frame from PiCamera2
+    frame = camera.capture_array()  # Capture image as a NumPy array
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)  # Convert to BGR for OpenCV
+
+    # Get frame dimensions
+    frameHeight, frameWidth, _ = frame.shape
+    xPixelLoc = -1
+    yPixelLoc = -1
+
+    # Process frame with MediaPipe
+    results = hands.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+    if results.multi_hand_landmarks:
+        for handLandmarks in results.multi_hand_landmarks:
+            for point in handsModule.HandLandmark:
+                if point == 0:  # Wrist
+                    wrist = handLandmarks.landmark[handsModule.HandLandmark.WRIST]
+                    print(f"Wrist (Landmark 0) - X: {wrist.x}, Y: {wrist.y}, Z: {wrist.z}")
+
+                    # Convert to pixel coordinates
+                    xPixelLoc = min(width * normX(1 - wrist.x), width)  # Fix mirroring
+                    yPixelLoc = min(height * norm(wrist.y), height)
+                    print(f"Pixel Location: x: {xPixelLoc}, y: {yPixelLoc}")
+
+                # Draw landmarks on frame
+                normalizedLandmark = handLandmarks.landmark[point]
+                pixelCoordinatesLandmark = drawingModule._normalized_to_pixel_coordinates(normalizedLandmark.x, normalizedLandmark.y, frameWidth, frameHeight)
+
+                cv2.circle(frame, pixelCoordinatesLandmark, 5, (0, 255, 0), -1)
+    else:
+        print('hidden hand')
+    # Display the frame
+    cv2.imshow('Hand Tracking', frame)
+
+    # Exit on 'ESC' key
+    if cv2.waitKey(1) == 27:
+        return
+    return xPixelLoc, yPixelLoc
+
+'''
+# Run hand tracking loop
+while True:
+    getXY()
+
+# Cleanup
+cv2.destroyAllWindows()
+camera.stop()
+'''
---
+++ project.py
@@ -0,0 +1,279 @@
+import os
+import gpiod
+from gpiod.line import Direction, Value
+from gpiozero import LED, DistanceSensor
+import time
+import threading
+from pynput.mouse import Button, Controller
+import smbus2
+import math
+from collections import deque
+# from testAccelerometer import * 
+from cam2 import *
+
+# GPIO chip and lines
+CHIP_NAME = "/dev/gpiochip0"
+PIN_LEFT = 17
+PIN_RIGHT = 27
+PIN_SCROLL = 22
+PIN_PICK = 23
+#DIP two-color LED (Dual In-line Package)
+PIN_DIP_RED = LED(20)
+PIN_DIP_GRN = LED(21)
+#SMD RGB LED (Surface Mount Device)
+PIN_SMD_RED = LED(26)
+PIN_SMD_GRN = LED(19)
+PIN_SMD_BLU = LED(13)
+#Ultrasonic
+PIN_TRIGGER = 6
+PIN_ECHO = 5
+# Accelerometer/Gyroscope
+MPU_ADDR = 0x68
+PWR_MGMT_1 = 0x6B
+ACCEL_XOUT_H = 0x3B
+GYRO_XOUT_H = 0x43
+
+ACCEL_THRESHOLD = 0.1  # Tune this value based on your sensor noise
+ACC_ERROR_X = 0.03#0.00328#-5.493426
+ACC_ERROR_Y= -0.03#0.030048#-3.049761
+GYRO_ERROR_X = -2.930811
+GYRO_ERROR_Y = 1.308454
+GYRO_ERROR_Z = 0.787824
+# AccErrorX: 0.000728
+# run body_functions every 0.01 seconds (10 milliseconds)
+TIME_INTERVAL = 0.01
+PIXEL_MOUSE_MOVE = 300
+
+mouse = Controller()
+bus = smbus2.SMBus(1)
+bus.write_byte_data(MPU_ADDR, PWR_MGMT_1, 0) # wake up the MPU6050
+
+class SharedVariable:
+    def __init__(self):
+        self.bProgramExit = 0
+        self.right = 0
+        self.left = 0
+        self.scroll = 0
+        self.pick = 1
+        self.rightLastClickTime = self.get_millis()
+        self.leftLastClickTime = self.get_millis()
+        self.scrollLastClick = self.get_millis()
+        self.pickLastClick = self.get_millis()
+        self.scroll_speed = 1
+        self.scroll_dir = -1
+        self.last_ax = deque([])
+        self.last_ay = deque([])
+        self.last_az = deque([])
+        self.vx = 0
+        self.vy = 0
+        self.vz = 0
+        self.n = 0
+        self.lastUltraTime = self.get_millis()
+    
+    def get_millis(self):
+        return int(time.time() * 1000)
+
+
+def read_gpio_value(line):
+    with gpiod.request_lines(CHIP_NAME, consumer="sensor", config={line: gpiod.LineSettings(direction = Direction.INPUT)}) as request:
+        return request.get_value(line)
+        
+def write_gpio_value(line, val):
+    with gpiod.request_lines(CHIP_NAME, consumer="sensor", config={line: gpiod.LineSettings(direction = Direction.OUTPUT)}) as request:
+        return request.set_value(line, val)
+
+def body_right(sv):
+    if sv.pick == 1:
+        curr_time = sv.get_millis()
+        if read_gpio_value(PIN_RIGHT) and (curr_time - sv.rightLastClickTime) > 400:
+            print("right click")
+            sv.right = 1
+            mouse.click(Button.right, 1)
+            sv.rightLastClickTime = sv.get_millis()
+        elif (curr_time - sv.rightLastClickTime) > 400:
+            sv.right = 0
+
+def body_left(sv):
+    if sv.pick == 1:
+        curr_time = sv.get_millis()
+        if read_gpio_value(PIN_LEFT) and (curr_time - sv.leftLastClickTime) > 400:
+            print("left click")
+            if sv.left == 1:
+                mouse.press(Button.left)
+            else:
+                mouse.click(Button.left, 1)
+            sv.left = 1
+            sv.leftLastClickTime = sv.get_millis()
+        elif (curr_time - sv.leftLastClickTime) > 400:
+            sv.left = 0
+            mouse.release(Button.left)
+
+def body_scroll(sv):
+    if sv.pick == 1:
+        curr_time = sv.get_millis()
+        if read_gpio_value(PIN_SCROLL) and (curr_time - sv.scrollLastClick) > 100:
+            print("scroll click")
+            sv.scroll = 1
+            sv.scrollLastClick = sv.get_millis()
+            mouse.scroll(0, sv.scroll_speed*sv.scroll_dir)
+        elif (curr_time - sv.scrollLastClick) > 400:
+            if sv.scroll == 1:
+                print("stop scroll click")
+            sv.scroll = 0
+
+def body_pick(sv):
+    curr_time = sv.get_millis()
+    if read_gpio_value(PIN_PICK) and (curr_time - sv.pickLastClick) > 400:
+        if sv.pick == 0:
+            print("pick up mouse")
+            sv.pick = 1
+            sv.pickLastClick = sv.get_millis()
+        else:
+            print("drop mouse")
+            sv.pick = 0
+            sv.pickLastClick = sv.get_millis()
+            
+def body_twocolor(sv):
+    if (sv.pick == 1):
+        PIN_DIP_RED.off()
+        PIN_DIP_GRN.on()
+    else :
+        PIN_DIP_RED.on()
+        PIN_DIP_GRN.off()
+
+def body_rgbcolor(sv):
+    if (sv.pick == 1) and (sv.left == 1):
+        PIN_SMD_RED.on()
+        PIN_SMD_GRN.on()
+        PIN_SMD_BLU.on()
+    else:
+        PIN_SMD_RED.off()
+        PIN_SMD_GRN.off()
+        PIN_SMD_BLU.off()
+
+def body_ultra(sv):
+    currTime = sv.get_millis()
+    if (currTime - sv.lastUltraTime) > 400 and sv.scroll == 0 and sv.pick == 1:
+        distance = sensor = DistanceSensor(echo=PIN_ECHO, trigger=PIN_TRIGGER).distance * 100
+        if distance < 7 or distance > 17:
+            sv.scroll_speed = 1
+        else:
+            print(f"The distance is: {distance:.2f} cm")
+            dist_10 = distance - 7
+            sv.scroll_speed = int(10/dist_10)
+        sv.lastUltraTime = currTime
+        
+
+def read_sensor_data():
+    # Read accelerometer and gyroscope data (14 bytes starting from address 0x3B)
+    data = bus.read_i2c_block_data(MPU_ADDR, 0x3B, 14)
+    
+    # Combine the high and low bytes for X, Y, Z accelerometer values
+    AcX = (data[0] << 8) + data[1]
+    AcY = (data[2] << 8) + data[3]
+    AcZ = (data[4] << 8) + data[5]
+    
+    return AcX, AcY, AcZ
+
+def calculate_angles(AcX, AcY, AcZ):
+    minVal = 265
+    maxVal = 402
+    # Map the accelerometer data to -90 to 90 degrees
+    xAng = map_value(AcX, minVal, maxVal, -90, 90)
+    yAng = map_value(AcY, minVal, maxVal, -90, 90)
+    zAng = map_value(AcZ, minVal, maxVal, -90, 90)
+
+    # Calculate angles using atan2 function
+    x = math.degrees(math.atan2(-yAng, -zAng) + math.pi)
+    y = math.degrees(math.atan2(-xAng, -zAng) + math.pi)
+    z = math.degrees(math.atan2(-yAng, -xAng) + math.pi)
+    
+    return x, y, z
+
+def map_value(value, in_min, in_max, out_min, out_max):
+    # Map a value from one range to another
+    return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+def body_tilt(sv):
+    if sv.pick == 1 and sv.scroll == 1:
+        curr_time = sv.get_millis()
+        if (curr_time - sv.scrollLastClick) > 400:
+            sv.scroll = 0
+        else:
+            AcX, AcY, AcZ = read_sensor_data()
+            x, y, z = calculate_angles(AcX, AcY, AcZ)
+            if z > 35 and z < 90:
+                print(f"tilted down, z: {z}")
+                sv.scroll_dir = -1
+            elif z < 15 or z >= 90:
+                print(f"tilted up, z: {z}")
+                sv.scroll_dir = 1
+
+
+def body_mouse(sv):
+    if sv.pick == 1:
+        xPixelLoc, yPixelLoc = getXY()
+        if xPixelLoc != -1:
+            mouse.position = (xPixelLoc, yPixelLoc)
+
+# def sensor_thread(func, sv):
+#     while not sv.bProgramExit:
+#         func(sv)
+#         time.sleep(TIME_INTERVAL)  # Small delay to reduce CPU usage
+
+def sensor_thread(func, sv, core_id):
+    """Runs the given function as a thread and pins it to a specific core."""
+    def target():
+        os.sched_setaffinity(0, {core_id})  # Set thread affinity to core_id
+        while not sv.bProgramExit:
+            func(sv)
+            time.sleep(TIME_INTERVAL)
+
+    t = threading.Thread(target=target)
+    t.start()
+    return t
+
+if __name__ == "__main__":
+    sv = SharedVariable()
+
+    '''threads = [
+        threading.Thread(target=sensor_thread, args=(body_right, sv)),
+        threading.Thread(target=sensor_thread, args=(body_left, sv)),
+        threading.Thread(target=sensor_thread, args=(body_scroll, sv)),
+        threading.Thread(target=sensor_thread, args=(body_pick, sv)),
+        threading.Thread(target=sensor_thread, args=(body_rgbcolor, sv)),
+        threading.Thread(target=sensor_thread, args=(body_twocolor, sv)),
+        threading.Thread(target=sensor_thread, args=(body_ultra, sv)),
+        threading.Thread(target=sensor_thread, args=(body_tilt, sv)),
+        #threading.Thread(target=sensor_thread, args=(body_speed, sv))
+    ]'''
+    core_0_threads = [
+        sensor_thread(body_right, sv, 0),
+        sensor_thread(body_left, sv, 0),
+        sensor_thread(body_scroll, sv, 0),
+        sensor_thread(body_pick, sv, 0),
+        sensor_thread(body_rgbcolor, sv, 0),
+        sensor_thread(body_twocolor, sv, 0),
+        sensor_thread(body_ultra, sv, 0),
+        sensor_thread(body_tilt, sv, 0),
+    ]
+
+    body_mouse_thread = sensor_thread(body_mouse, sv, 1)
+    
+    # for t in threads:
+    #     t.start()
+    
+    try:
+        while not sv.bProgramExit:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        sv.bProgramExit = 1
+        
+    # Join threads
+    for t in core_0_threads + [body_mouse_thread]:
+        t.join()
+    
+    cv2.destroyAllWindows()
+    camera.stop()
+    
+    print("Program finished.")
